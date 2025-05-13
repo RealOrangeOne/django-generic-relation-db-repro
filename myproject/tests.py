ed411add206eaac0548f1f904e518d8da1c8abb3
Jake Howard <jake.howard@torchbox.com>
2025-05-13 16:25:15 +0100

    Add test for explicit DB usage

--- myproject/tests.py
+++ myproject/tests.py
@@ -17,12 +17,28 @@ class TestTestCase(TransactionTestCase):
         self.revision = Revision.objects.first()
 
     def test_setup(self):
-        self.assertEqual(self.revision.mains.db, "replica")
         self.assertEqual(self.revision._state.db, "replica")
+
+        # This is correct, as Django assumes reads by default
+        self.assertEqual(self.revision.mains.db, "replica")
 
     def test_update(self):
         with CaptureQueriesContext(connections["replica"]) as replica_queries:
             self.revision.mains.update(text="test")
+
+        # Replica should not be doing write queries
+        self.assertEqual(replica_queries.captured_queries, [])
+
+    def test_explicit(self):
+        revision = Revision.objects.using("default").first()
+
+        self.assertEqual(revision._state.db, "default")
+
+        # This is correct, as Django assumes reads by default
+        self.assertEqual(revision.mains.db, "replica")
+
+        with CaptureQueriesContext(connections["replica"]) as replica_queries:
+            revision.mains.update(text="test")
 
         # Replica should not be doing write queries
         self.assertEqual(replica_queries.captured_queries, [])
